c42d36ad0f75e1a5be481fa4e2f367c7829ace41
Kevin Rinaldi <kevin@ebizmarts.com>
2019-05-16 22:17:35 -0300

    Remover texto de netbeans en aves

--- Obligatorio1P2/src/obligatorio1p2/Aves.java
+++ Obligatorio1P2/src/obligatorio1p2/Aves.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package obligatorio1p2;
 
-/**
- *
- * @author Ezequiel
- */
 import java.util.*;
 
 public class Aves {
